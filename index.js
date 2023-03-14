754973b197d6d223195f82b7f56e0c5e27e3be51
giorgi kiladze <giorgikiladze92@gmail.com>
2023-03-14 05:58:17 -0700

    project finished

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const questions = document.querySelectorAll(".line-p")
 const answers = document.querySelectorAll(".answer")
 const arrowDowns = document.querySelectorAll(".arrow-down")
-
+let active;
 
 for (let index = 0; index < questions.length; index++) {
   questions[index].addEventListener("click",function(){
+    if ((active || active === 0) && active !== index) {
+      answers[active].classList.remove("show")
+      arrowDowns[active].classList.remove("arrow-up")
+      questions[active].classList.remove("line-bold")
+    }
+
+    active = index
     answers[index].classList.toggle("show")
     arrowDowns[index].classList.toggle("arrow-up")
     questions[index].classList.toggle("line-bold")
 
-    for (let index1 = 0; index1 < questions.length; index1++) {
-      if (index !== index1 && answers[index].classList === "show") {
-        answers[index].classList.toggle("answer")
-      } else {
-        
-      }
-      
-    }
+   
   })
 
   
 
   
 }
+
 
